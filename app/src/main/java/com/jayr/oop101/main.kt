d45a8a1566c07d0950a686c1af24719778fa6f87
JosephRidge <josephridge18@gmail.com>
2025-06-23 16:43:33 +0300

    update on OOP

--- app/src/main/java/com/jayr/oop101/main.kt
+++ app/src/main/java/com/jayr/oop101/main.kt
@@ -1,7 +1,10 @@
 package com.jayr.oop101
 
+import android.graphics.Rect
 import java.util.Calendar
+import kotlin.math.PI
 
+/*
 /*
 * Class(blueprint to build our object):
 * - keyword: class
@@ -103,16 +106,144 @@ class Example: D(), A, B {
         println("Example")    }
 
 }
-fun main(){
-    var parent: Person = Person(username="John Doe", surname = "Does", age = 50) // parent
-    var parent2: Person = Person(username="John Doe", surname = "Does", age = 50) // parent
-    var individual1:Person = Person(name = "Bob", guardian = parent)// parent
-    parent.welcomePerson().also(::println)
-    parent.currentYear.also(::println)
 
-    println(parent2 == parent)
-    println(parent2)
-    println(parent)
+
+*/
+
+
+/*
+* class(blueprint to create a real world object)... objects(instance of a class)  // theoretical
+* keyword: class
+* name:
+* constructor(primary, secondary)
+* body
+* */
+
+class SteamVehicle // empty class
+
+
+class Rectangle(var width:Float, var height:Float):Shape(){
+    override fun area(): Float {
+        return width*height
+    }
+
+    override fun circumference(): Float {
+        return 0f
+    }
+
+
+}
+
+class Circle(var radius:Float):Shape(){
+    override fun area(): Float {
+        return (PI * radius * radius).toFloat()
+    }
+
+    override fun circumference(): Float {
+        return (PI *  (2 * radius)).toFloat()
+    }
+
+}
+// rule make sure that all shapes implement the following( circumference, area)
+// it does ont retain state (those entities that hold data)
+//interface Shape{
+//    fun area():Float
+//    fun circumference():Float
+//}
+
+// abstract class - able ot store state but you cannot instanciate
+abstract class Shape{
+    abstract fun area():Float
+    abstract fun circumference():Float
+}
+class Cylinder(var radius:Float):Density() {
+
+}
+
+open class Density(){
+    var rad:Double = 0.0
+    var height: Double = 0.0
+
+    fun volume(): Double {
+        return ((PI * rad * rad)* height)
+    }
+}
+//data class : it is used for handling data itself
+data class User(var name:String, var age:Int)
+
+// store constants efficiently
+enum class Country(var code:String){
+    GERMANY("GER"), KENYA("KEN"), SENEGAL("SEN"), CONGO("DRC")
+}
+enum class Gender{
+    MALE, FEMALE
+}
+var rect1:Rectangle = Rectangle(width = 10f, height = 12f)
+//    var rect2:Rectangle = Rectangle(width = 12f, height = 14f)
+//    var circle:Circle = Circle(radius = 21f)
+//
+//    var cylinder:Cylinder = Cylinder(radius = 21.0f)
+//    cylinder.volume()
+//
+//    var user: User = User(name = "John Doe", age =12)
+//    var user2: User = User(name = "John Doe", age =12)
+//    var (name, age) = user // object destructuring
+//    print("My name is $name and I am $age years old.")
+fun main(){
+
+    fun nameOfFunction(){
+        println("hellow world")
+    }
+    // lambda function : anonymous functions =>
+//    var count = {number:Int ->
+//         println(number)
+//    }
+//    count(10)
+
+    fun danceInGerman(vararg name: String){
+
+    }
+//
+    for (country in Country.entries){
+        when(country){
+            Country.GERMANY -> danceInGerman()
+            Country.KENYA -> "Habaru"
+            Country.SENEGAL -> "Bonjour"
+            Country.CONGO -> "Bonjour"
+        }
+    }
+
+    /*
+    * COLLECTIONS:
+    * - hold multiple entities inside a container
+    * - by default all collections are immutable but you can make the collection mutable using a specific keyword
+    * - List (ordered and u can access each entity using an index)
+    * - Set(unique elements)
+    * - Map(key .. value )
+    *
+    * */
+
+    var fruitList: List<String> = listOf("apple", "mango","mint","pineapple","pineapple", "mango")
+    fruitList.forEach { fruit ->
+//        println(fruit)
+    }
+
+    var fruitSet: Set<String> = setOf("apple", "mango","mint","pineapple","pineapple", "mango")
+    fruitSet.forEach { fruit ->
+//        println(fruit)
+    }
+
+    var fruitMap:Map<String, String> = mapOf(
+        "a" to "apple",
+        "b" to "banana",
+        "c" to "cherries",
+           )
+    fruitMap.forEach { fruit ->
+        println(fruit)
+    }
+
+
+
 }
 
 
